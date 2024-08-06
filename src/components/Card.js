5ecf76b860b573e20ad7a4dd075825a0e363309b
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-08-06 18:11:24 +0530

    prettier applied

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,36 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 function Card(props) {
-    const ingredientLines = props.recipe.ingredientLines.map((ingredientLine, index) => {
-        return <li key={index} className="list-group-item">{ingredientLine}</li>
-    })
-    return (
-        <div className="card" style={{ width: "18rem" }}>
-            <img src={props.recipe.image} className="card-img-top" alt={props.recipe.label} />
-            <div className="card-body">
-                <h5 className="card-title">{props.recipe.label}</h5>
-            </div>
-            <ul className="list-group list-group-flush">
-                {ingredientLines}
-            </ul>
-        </div>
-    );
+  const ingredientLines = props.recipe.ingredientLines.map(
+    (ingredientLine, index) => {
+      return (
+        <li key={index} className="list-group-item">
+          {ingredientLine}
+        </li>
+      );
+    }
+  );
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img
+        src={props.recipe.image}
+        className="card-img-top"
+        alt={props.recipe.label}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.recipe.label}</h5>
+      </div>
+      <ul className="list-group list-group-flush">{ingredientLines}</ul>
+    </div>
+  );
 }
 
 export default Card;
 
 Card.propTypes = {
-    recipe: PropTypes.shape({
-        image: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        ingredientLines: PropTypes.array.isRequired
-    })
-
-}
+  recipe: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    ingredientLines: PropTypes.array.isRequired,
+  }),
+};
